bc3f117c5beef370f640699821e9c90fb8195917
Bazhe <67385817+Baze-cmd@users.noreply.github.com>
2024-11-07 02:46:10 +0100

    imporved

--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ import pandas as pd
 from datetime import datetime
 import os
 import re
+from tqdm import tqdm
 
 
+# handles data scraping for that code from date_from to date_to
 def get_from_to(date_from, date_to, code):
-    url = f"https://www.mse.mk/mk/stats/symbolhistory/{code}"
+    url = f"https://www.mse.mk/en/stats/symbolhistory/{code}"
 
     payload = f"FromDate={date_from}&ToDate={date_to}&Code={code}"
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
@@ -41,16 +43,23 @@ def get_from_to(date_from, date_to, code):
     return pd.DataFrame(rows_data, columns=headers)
 
 
+# TODO: this function should return a list of DataFrame objects from the date passed in up to the current date
+# The program should work if run for the first time but not after a second time
+def get_recent(code, date):
+    return None
+
+
 def one_year_before(date):
-    date_format = "%d.%m.%Y"
+    date_format = "%m/%d/%Y"
     given_date = datetime.strptime(date, date_format)
     one_year_ago = given_date.replace(year=given_date.year - 1)
     return one_year_ago.strftime(date_format)
 
 
-def get_data_for(code):
+# gets last 10 years of data for that code and returns a list of Data Frames
+def get_last_ten_years(code):
     data_frame_list = []
-    date_today = datetime.now().strftime("%d.%m.%Y")
+    date_today = datetime.now().strftime("%m/%d/%Y")
     date_to = date_today
     date_from = one_year_before(date_to)
     for i in range(10):
@@ -63,21 +72,58 @@ def get_data_for(code):
     return data_frame_list
 
 
-def scrape_data(codes):
-    output_dir = os.path.join(os.getcwd(), 'data')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    for code in codes:
-        df_list = get_data_for(code)
+# scrapes data for each publisher and returns a map where key is code and value is a Data Frame
+# TODO: make a function that appends the data frames to the CSV files in /data
+def scrape_data(last_date_map):
+    # output_dir = os.path.join(os.getcwd(), 'data')
+    # if not os.path.exists(output_dir):
+    #    os.makedirs(output_dir)
+
+    for code, date in tqdm(last_date_map.items(), ncols=100, colour='green'):
+        df_list = []
+        if date is None:
+            df_list = get_last_ten_years(code)
+        else:
+            df_list = get_recent(code, date)
         result = df_list[0]
         for i in range(1, len(df_list)):
             result = pd.concat([result, df_list[i]], ignore_index=True)
-        full_path = os.path.join(output_dir, f'{code}.csv')
-        result.to_csv(full_path, index=False)
+        result['Date'] = pd.to_datetime(result['Date'], format='%m/%d/%Y')
+        result['Date'] = result['Date'].dt.strftime('%d.%m.%Y')
+        last_date_map[code] = result
+
+    return last_date_map
+        # full_path = os.path.join(output_dir, f'{code}.csv')
+        # last_date_map.to_csv(full_path, index=False)
 
 
+# checks the last retrieved date in CSV files for each publisher and returns a map where key is the code and value is latest date
+def get_latest_date(codes):
+    result = {}
+    path = os.path.join(os.getcwd(), "data")
+
+    for code in codes:
+        file_path = os.path.join(path, f"{code}.csv")
+        if not os.path.exists(file_path):
+            result[code] = None
+            continue
+
+        df = pd.read_csv(file_path)
+
+        if df.empty:
+            result[code] = None
+            continue
+
+        df['Date'] = pd.to_datetime(df['Date'], format='%d.%m.%Y', errors='coerce')
+        latest_date = df['Date'].max()
+        result[code] = latest_date.strftime('%d.%m.%Y')
+
+    return result
+
+
+# gets the code of all valid publishers and returns them as a list of strings
 def get_codes():
-    url = "https://www.mse.mk/mk/stats/symbolhistory/ADIN"
+    url = "https://www.mse.mk/en/stats/symbolhistory/ADIN"
     response = requests.get(url)
     if response.status_code != 200:
         print("Bad response code")
@@ -104,8 +150,8 @@ def get_codes():
 
 def main():
     codes = get_codes()
-    scrape_data(codes)
-
+    last_date_map = get_latest_date(codes)
+    data = scrape_data(last_date_map)
 
 if __name__ == "__main__":
     main()
